0f41563e1445b05e7a6f6ffacf378806a225c8d8
nikonikolov <niko.b.nikolov@gmail.com>
2015-09-01 22:48:34 +0000

    Fixed cards hover

--- subjects/script.js
+++ subjects/script.js
@@ -5,29 +5,18 @@ $(document).ready( function() {
 
 	$('.card').hover( 
 
-		/*function(){
-			$(this).find('div.card_img').toggleClass('card_img_hover');
+		function(){
+			mask= $(this).find(".mask");
+			button = $(this).find('h5');
+
+			$(mask).toggleClass('mask_hover');
 			$(this).toggleClass('card_hover');
+			$(button).toggleClass('h5_hover');
 		},
 
 		function(){
-			$(this).find('div.card_img').toggleClass('card_img_hover');
+			$(mask).toggleClass('mask_hover');
 			$(this).toggleClass('card_hover');
-		}
-			*/
-		
-		function(){
-			read_img_url = $(this).find('.card_img').css("background-image");
-			val_img_url = read_img_url.replace(/["']/g, "");
-
-			$(this).find('.card_img').css("background", "linear-gradient(rgba(255,255,255, 0.3),rgba(255,255,255, 0.3)), " + val_img_url);
-			$(this).toggleClass('card_hover');
-		},
-
-		function(){
-			$(this).find('.card_img').css("background", "linear-gradient(rgba(255,255,255, 0.0),rgba(255,255,255, 0.0)), " + val_img_url);
-			$(this).toggleClass('card_hover');
+			$(button).toggleClass('h5_hover');
 	});
-
-
 });
