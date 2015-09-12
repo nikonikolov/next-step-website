cae72a2efa1f0112cf8c852aacec1970b572bc10
nikonikolov <niko.b.nikolov@gmail.com>
2015-09-12 22:29:15 +0000

    temporary commit

--- university/script.js
+++ university/script.js
@@ -12,6 +12,28 @@ $(document).ready( function() {
 			$(mask).toggleClass('mask_hover');
 			$(this).toggleClass('card_hover');
 			$(button).toggleClass('h5_hover');
+
+			$(this).click( 
+
+		function(){
+			mask= $(this).find(".mask");
+			button1 = $(this).find('h5');
+			$(button).toggleClass('h5_hover');
+			$(button).toggleClass('h5_click');
+			 setTimeout(function() {
+			$(button).toggleClass('h5_click');
+       		 
+       		 $(button).toggleClass('h5_hover');
+   			 }, 1000);
+			//$(button).delay("slow").toggleClass('h5_hover');
+
+			//$(button).delay(1000).toggleClass('h5_click');
+			//$(button1).toggleClass('h5_click').delay(5000).toggleClass('h5_click');
+			//$(button1).addClass('h5_click').delay(5000).removeClass('h5_click');
+			
+		});
+
+			
 		},
 
 		function(){
@@ -19,4 +41,22 @@ $(document).ready( function() {
 			$(this).toggleClass('card_hover');
 			$(button).toggleClass('h5_hover');
 	});
+
+	/*$('.card').click( 
+
+		function(){
+			mask= $(this).find(".mask");
+			button1 = $(this).find('h5');
+			$(button).toggleClass('h5_hover');
+			$(button).toggleClass('h5_click');
+			//$(button).delay("slow").toggleClass('h5_hover');
+
+			//$(button).delay(1000).toggleClass('h5_click');
+			//$(button1).toggleClass('h5_click').delay(5000).toggleClass('h5_click');
+			//$(button1).addClass('h5_click').delay(5000).removeClass('h5_click');
+			
+		});*/
+
+	
+
 });
