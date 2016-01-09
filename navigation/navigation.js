7af15d0425221c96b524d21825e94da193dd97ac
nikonikolov <niko.b.nikolov@gmail.com>
2016-01-09 18:24:02 +0000

    new navigation bar, electronics started

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,3 +1,6 @@
 $(document).ready( function () {	
-	$("#navigation").load("/navigation/navigation.html");	
+	//$("#navigation").load("/navigation/navigation.html");	
+	$("#navigation").load("/navigation/navigation.html", function() {
+    	$.getScript("/navigation/navigation_custom.js");
+	});	
 });
